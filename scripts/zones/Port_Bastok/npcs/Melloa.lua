b734f64623155da08d07de7129057be008bb72d9
claywar <claywar@lsb.dev>
2025-01-20 12:46:51 -0500

    Remove empty NPCs, add Default Actions - Port Bastok

--- scripts/zones/Port_Bastok/npcs/Melloa.lua
+++ scripts/zones/Port_Bastok/npcs/Melloa.lua
@@ -29,9 +29,6 @@ entity.onSpawn = function(npc)
     npc:pathThrough(pathNodes, xi.path.flag.PATROL)
 end
 
-entity.onTrade = function(player, npc, trade)
-end
-
 entity.onTrigger = function(player, npc)
     local stock =
     {
@@ -50,12 +47,6 @@ entity.onTrigger = function(player, npc)
 
     player:showText(npc, ID.text.MELLOA_SHOP_DIALOG)
     xi.shop.nation(player, stock, xi.nation.BASTOK)
-end
-
-entity.onEventUpdate = function(player, csid, option, npc)
-end
-
-entity.onEventFinish = function(player, csid, option, npc)
 end
 
 return entity
