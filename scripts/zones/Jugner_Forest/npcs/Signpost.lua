b5a24c04fc3676597340de769eb22822d51fe6e2
claywar <claywar@lsb.dev>
2025-01-25 10:45:57 -0500

    Remove empty NPCs, add Default Actions - Gustav ~ Kamihr

--- scripts/zones/Jugner_Forest/npcs/Signpost.lua
+++ scripts/zones/Jugner_Forest/npcs/Signpost.lua
@@ -6,9 +6,6 @@
 ---@type TNpcEntity
 local entity = {}
 
-entity.onTrade = function(player, npc, trade)
-end
-
 entity.onTrigger = function(player, npc)
     local xPos = player:getXPos()
     local zPos = player:getZPos()
@@ -52,9 +49,6 @@ entity.onTrigger = function(player, npc)
     else
         print('Unknown Signpost')
     end
-end
-
-entity.onEventUpdate = function(player, csid, option, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
