73bcaf11f24ca4f789885d0b44fe98910666ec50
claywar <claywar@lsb.dev>
2025-01-26 14:37:22 -0500

    Remove empty NPCs, add Default Actions - Bastok Mines ~ Buburimu

--- scripts/zones/Beaucedine_Glacier_[S]/npcs/Watchful_Pixie.lua
+++ scripts/zones/Beaucedine_Glacier_[S]/npcs/Watchful_Pixie.lua
@@ -6,17 +6,9 @@
 ---@type TNpcEntity
 local entity = {}
 
-entity.onTrade = function(player, npc, trade)
-end
-
 entity.onTrigger = function(player, npc)
+    -- TODO: Conflict with default action
     player:startEvent(4002)
-end
-
-entity.onEventUpdate = function(player, csid, option, npc)
-end
-
-entity.onEventFinish = function(player, csid, option, npc)
 end
 
 return entity
--- scripts/zones/Bibiki_Bay/npcs/Pohka_Chichiyowahl.lua
+++ scripts/zones/Bibiki_Bay/npcs/Pohka_Chichiyowahl.lua
@@ -8,9 +8,6 @@ local ID = zones[xi.zone.BIBIKI_BAY]
 ---@type TNpcEntity
 local entity = {}
 
-entity.onTrade = function(player, npc, trade)
-end
-
 entity.onTrigger = function(player, npc)
     local stock =
     {
@@ -21,12 +18,6 @@ entity.onTrigger = function(player, npc)
 
     player:showText(npc, ID.text.POHKA_SHOP_DIALOG)
     xi.shop.nation(player, stock, xi.nation.WINDURST)
-end
-
-entity.onEventUpdate = function(player, csid, option, npc)
-end
-
-entity.onEventFinish = function(player, csid, option, npc)
 end
 
 return entity
