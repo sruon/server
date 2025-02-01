075a18650ad11ab7f22f687be0aaf51b7ea5c269
KnowOne <knowone134@gmail.com>
2025-02-01 15:27:19 -0600

    Quest The Die is Cast

--- scripts/zones/Arrapago_Reef/mobs/Bukki.lua
+++ scripts/zones/Arrapago_Reef/mobs/Bukki.lua
@@ -2,10 +2,33 @@
 -- Area: Arrapago Reef
 --   NM: Bukki
 -----------------------------------
-mixins = { require('scripts/mixins/families/imp') }
+mixins =
+{
+    require("scripts/mixins/job_special"),
+    require("scripts/mixins/families/imp"),
+}
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
+
+entity.onMobSpawn = function(mob)
+    dsp.mix.jobSpecial.config(mob, {
+        hpp = math.random(10, 50),
+    })
+    mob:setTP(3000)
+    mob:SetMobSkillAttack(0)
+    mob:SetMagicCastingEnabled(true)
+    mob:setMobMod(dsp.mobMod.IDLE_DESPAWN, 300)
+    mob:setMobMod(dsp.mobMod.GIL_MAX, -1)
+    mob:setLocalVar("tp_spam", math.random(5, 15))
+    mob:addListener("COMBAT_TICK", "BUKKI_TICK", function(mob)
+        if mob:getHPP() <= mob:getLocalVar("tp_spam") and mob:getLocalVar("tp_spam") > 0 then
+            mob:setLocalVar("tp_spam", 0)
+            mob:SetMagicCastingEnabled(false)
+            mob:SetMobSkillAttack(3002)
+        end
+    end)
+end
 
 entity.onMobDeath = function(mob, player, optParams)
 end
