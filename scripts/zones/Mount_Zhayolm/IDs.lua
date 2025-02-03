118c3d5bf5f75938e1c4ef931a3aa078759e110a
KnowOne <Knowone134@gmail.com>
2025-02-02 21:08:50 -0600

    add quest Embers of His Past

--- scripts/zones/Mount_Zhayolm/IDs.lua
+++ scripts/zones/Mount_Zhayolm/IDs.lua
@@ -24,6 +24,7 @@ zones[xi.zone.MOUNT_ZHAYOLM] =
         CANNOT_LEAVE                  = 7332, -- You cannot leave this area while in the possession of <keyitem>.
         RESPONSE                      = 7341, -- There is no response...
         YOU_HAVE_A_BADGE              = 7354, -- You have a %? Let me have a closer look at that...
+        NOTHING_OUT_OF_ORDINARY       = 7375, -- There is nothing out of the ordinary here.
         HAND_OVER_TO_IMMORTAL         = 7428, -- You hand over the % to the Immortal.
         YOUR_IMPERIAL_STANDING        = 7429, -- Your Imperial Standing has increased!
         MINING_IS_POSSIBLE_HERE       = 7430, -- Mining is possible here if you have <item>.
