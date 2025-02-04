fc8bc7a41886b9aab26e526a92eacafd81caa324
KnowOne <knowone134@gmail.com>
2025-02-04 15:45:33 -0600

    salvage misc

--- scripts/zones/Bhaflau_Remnants/Zone.lua
+++ scripts/zones/Bhaflau_Remnants/Zone.lua
@@ -5,11 +5,21 @@
 local zoneObject = {}
 
 zoneObject.onInitialize = function(zone)
+    zone:registerTriggerArea(1,   340, 4, -420, 0, 0, 0)
+    zone:registerTriggerArea(2,   260, 4,  300, 0, 0, 0)
+    zone:registerTriggerArea(3,   300, 4,   60, 0, 0, 0)
+    zone:registerTriggerArea(4,   420, 4,  300, 0, 0, 0)
+    zone:registerTriggerArea(5,   380, 4,   60, 0, 0, 0)
+    zone:registerTriggerArea(6,  -460, 4, -500, 0, 0, 0)
+    zone:registerTriggerArea(7,  -220, 4, -500, 0, 0, 0)
+    zone:registerTriggerArea(8,  -340, 4,   60, 0, 0, 0)
+    zone:registerTriggerArea(9,  -380, 4,  380, 0, 0, 0)
+    zone:registerTriggerArea(10, -300, 4,  380, 0, 0, 0)
 end
 
 zoneObject.onInstanceZoneIn = function(player, instance)
     if player:getInstance() == nil then
-        player:setPos(0, 0, 0, 0, 72)
+        player:setPos(620, 0, -260.640, 72, 72)
         return
     end
 
@@ -17,9 +27,8 @@ zoneObject.onInstanceZoneIn = function(player, instance)
     if pos.x == 0 and pos.y == 0 and pos.z == 0 then
         local entrypos = instance:getEntryPos()
         player:setPos(entrypos.x, entrypos.y, entrypos.z, entrypos.rot)
+        player:startEvent(101)
     end
-
-    player:addTempItem(5400)
 end
 
 zoneObject.onTriggerAreaEnter = function(player, triggerArea)
